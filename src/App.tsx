48ccd1c24da05a9c44e138b0b97ac88a1a3bb3f1
Mosamat Sabiha Shaikh <shkmos004@myuct.ac.za>
2024-10-07 13:24:25 +0200

    added buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import './App.scss';
+import './App.scss';  // Ensure this is imported to style the layout
 import Menu from './components/Menu';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Menu />
+      <div className="right-buttons">
+        <button>Share</button>
+        <button>Version</button>
+      </div>
     </div>
   );
 };
